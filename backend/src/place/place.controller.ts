b624e942565df2f6fe95811410f7d0fe5b8e61be
lahatra3 <lahatrap20.aps2a@gmail.com>
2022-11-27 01:03:46 +0300

    find all place...

--- backend/src/place/place.controller.ts
+++ backend/src/place/place.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { PlaceService } from './place.service';
 
 @Controller('place')
-export class PlaceController {}
+export class PlaceController {
+    constructor(
+        private placeService: PlaceService
+    ) {}
+
+    @UseGuards(AuthGuard('jwtFakoy'))
+    @Post('create')
+    async createPlace() {
+        return await this.createPlace();
+    }
+
+    @Get('all')
+    async findallPlace() {
+        return await this.placeService.findall();
+    }
+}
