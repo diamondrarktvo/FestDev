066ccc94b1ec04d62008875aa98ccaed3665ab85
lahatra3 <lahatrap20.aps2a@gmail.com>
2022-11-26 23:05:56 +0300

    fix bug...

--- backend/src/utilisateur/utilisateur.module.ts
+++ backend/src/utilisateur/utilisateur.module.ts
@@ -3,9 +3,15 @@ import { UtilisateurService } from './utilisateur.service';
 import { UtilisateurController } from './utilisateur.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Utilisateur } from 'src/entities/Utilisateur';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Utilisateur])],
+  imports: [
+    TypeOrmModule.forFeature([Utilisateur]),
+    ConfigModule.forRoot({
+      envFilePath: '.env'
+    }),
+],
   providers: [UtilisateurService],
   controllers: [UtilisateurController]
 })
