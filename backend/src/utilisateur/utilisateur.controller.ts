d491e57ed9663894a998f0d5a9fe0745d3ffb638
lahatra3 <lahatrap20.aps2a@gmail.com>
2022-11-26 13:55:07 +0300

    API for creating UTILISATEUR...

--- backend/src/utilisateur/utilisateur.controller.ts
+++ backend/src/utilisateur/utilisateur.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, NotAcceptableException, Post } from '@nestjs/common';
+import { CreateUtilisateurDto } from './dto/utilisateur.dto';
+import { UtilisateurService } from './utilisateur.service';
 
 @Controller('utilisateur')
-export class UtilisateurController {}
+export class UtilisateurController {
+    constructor(
+        private readonly utilisateurService: UtilisateurService
+    ) {}
+
+    @Post('create')
+    async createUtilisateur(@Body() donnees: CreateUtilisateurDto) {
+        if(!donnees) throw new NotAcceptableException('Credentials incorrects !');
+        return await this.utilisateurService.create(donnees);
+    }
+}
--- backend/src/utilisateur/utilisateur.service.ts
+++ backend/src/utilisateur/utilisateur.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Utilisateur } from 'src/entities/Utilisateur';
+import { Repository } from 'typeorm';
+import { CreateUtilisateurDto } from './dto/utilisateur.dto';
 
 @Injectable()
-export class UtilisateurService {}
+export class UtilisateurService {
+    constructor(
+        @InjectRepository(Utilisateur)
+        private utilisateurRepository: Repository<Utilisateur>
+    ) {}
+
+    async create(donnees: CreateUtilisateurDto): Promise<void> {
+        await this.utilisateurRepository
+        .createQueryBuilder()
+        .insert()
+        .into(Utilisateur)
+        .values({
+            nom: donnees.nom,
+            prenom: donnees.prenom,
+            quartier: donnees.quartier,
+            cin: donnees.cin,
+            username: donnees.username,
+            phone: donnees.phone,
+            mdp: donnees.password
+        })
+        .execute();
+    }
+}
+
+
