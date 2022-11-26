a99c4f7ff9efe924c9c1e2bf92b4d00f31046121
lahatra3 <lahatrap20.aps2a@gmail.com>
2022-11-26 13:55:07 +0300

    API for creating UTILISATEUR...

---
+++ backend/src/utilisateur/dto/utilisateur.dto.ts
@@ -0,0 +1,9 @@
+export class CreateUtilisateurDto {
+    nom: string;
+    prenom: string;
+    quartier: string;
+    cin: string;
+    username: string;
+    phone: string;
+    password: string;
+}
--- backend/src/utilisateur/utilisateur.module.ts
+++ backend/src/utilisateur/utilisateur.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UtilisateurService } from './utilisateur.service';
 import { UtilisateurController } from './utilisateur.controller';
-import { UtilisateurController } from './utilisateur.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Utilisateur } from 'src/entities/Utilisateur';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Utilisateur])],
   providers: [UtilisateurService],
   controllers: [UtilisateurController]
 })
