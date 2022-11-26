33b4990c536b8c1efa3265a861f1648eca42e9d1
lahatra3 <lahatrap20.aps2a@gmail.com>
2022-11-26 14:12:26 +0300

    api for finding UTILISATEUR...

--- backend/src/utilisateur/dto/utilisateur.dto.ts
+++ backend/src/utilisateur/dto/utilisateur.dto.ts
@@ -1,9 +1,29 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class CreateUtilisateurDto {
+    @ApiProperty()
     nom: string;
+
+    @ApiProperty()
     prenom: string;
+
+    @ApiProperty()
     quartier: string;
+
+    @ApiProperty()
     cin: string;
+
+    @ApiProperty()
     username: string;
+
+    @ApiProperty()
     phone: string;
+
+    @ApiProperty()
     password: string;
+}
+
+export class ParamUtilisateurDto {
+    @ApiProperty()
+    utilisateur_id: number
 }
--- backend/src/utilisateur/utilisateur.controller.ts
+++ backend/src/utilisateur/utilisateur.controller.ts
@@ -1,5 +1,7 @@
-import { Body, Controller, NotAcceptableException, Post } from '@nestjs/common';
-import { CreateUtilisateurDto } from './dto/utilisateur.dto';
+import { Body, Controller, Get, NotAcceptableException, 
+    Param, Post, Request, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { CreateUtilisateurDto, ParamUtilisateurDto } from './dto/utilisateur.dto';
 import { UtilisateurService } from './utilisateur.service';
 
 @Controller('utilisateur')
@@ -12,5 +14,23 @@ export class UtilisateurController {
     async createUtilisateur(@Body() donnees: CreateUtilisateurDto) {
         if(!donnees) throw new NotAcceptableException('Credentials incorrects !');
         return await this.utilisateurService.create(donnees);
+    }
+
+    @Get('all')
+    async findallUtilisateur() {
+        return await this.utilisateurService.findall();
+    }
+
+    @Get(':utilisateur_id')
+    async findUtilisateurById(@Param() donnees: ParamUtilisateurDto) {
+        if(!donnees) throw new NotAcceptableException('Credentials incorrects !');
+        return await this.utilisateurService.find(donnees);
+    }
+
+    @UseGuards(AuthGuard('jwtFakoy'))
+    @Get('')
+    async findUtilisateur(@Request() req: any) {
+        const donnees = {utilisateur_id: +req.user.id};
+        return await this.utilisateurService.find(donnees);
     }
 }
